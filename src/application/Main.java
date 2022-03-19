b03abdf39265658031b04c706698a0faf2707ed9
Jim <jimlotruglio@yahoo.com>
2022-03-18 19:31:14 -0600

    commit two

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -9,6 +10,7 @@ import javafx.scene.layout.BorderPane;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		 FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main-Ui.fxml"));
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
