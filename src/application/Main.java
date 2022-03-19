317508fcd5f646dc158e2c58d1103d7393ad7262
iantaiahn <iantaiahn@DESKTOP-7NC019E.lan>
2022-03-18 22:44:20 -0600

    Test for which branch this update will get pushed too.

--- src/application/Main.java
+++ src/application/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		System.out.println("Program has launched");
+		System.out.println("Which branch will this show up in?");
 		launch(args);
 	}
 }
